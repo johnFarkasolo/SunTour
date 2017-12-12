0cf234260f5501435833db249d18ddcc9ee6af09
Ivan Yarovyi <farkasolo@mail.ru>
2017-12-12 10:45:48 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,11 @@
+// $(document).ready(function() {
+
+// 	function heightDetect() {
+// 		$(".tour-bg img").css("height", $(window).height());
+// 	};
+// 	heightDetect();
+// 	$(window).resize(function() {
+// 		heightDetect();
+// 	});
+
+// }); 
